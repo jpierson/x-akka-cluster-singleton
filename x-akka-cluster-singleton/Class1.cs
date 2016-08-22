322ccc10352974b49c5fa97fa7c22f0fa91c8203
Jeff <jeffrey.pierson@gmail.com>
2016-08-22 09:42:13 -0400

    Add project files.

---
+++ x-akka-cluster-singleton/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace x_akka_cluster_singleton
+{
+    public class Class1
+    {
+    }
+}
