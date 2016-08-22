92ba45ffa9102acbb17d1d21b251d31eaf1386c6
Jeff <jeffrey.pierson@gmail.com>
2016-08-22 10:59:11 -0400

    Add initial files attempting to establish a cluster singleton.

---
+++ ClusterNode/MySingleton.cs
@@ -0,0 +1,22 @@
+﻿using Akka.Actor;
+using System;
+
+namespace ClusterNode
+{
+    public class MySingleton : ReceiveActor
+    {
+
+        public MySingleton()
+        {
+            Console.WriteLine("Singleton started");
+
+            ReceiveAny(e =>
+            {
+                var originalColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine($"Message received \"{e} \"");
+                Console.ForegroundColor = originalColor;
+            });
+        }
+    }
+}
---
+++ ClusterNode/Program.cs
@@ -0,0 +1,49 @@
+﻿using Akka.Actor;
+using Akka.Cluster.Tools.Singleton;
+using Akka.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClusterNode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var system = ActorSystem.Create("singletontest", ConfigurationFactory.Load().WithFallback(ClusterSingletonManager.DefaultConfig()));
+            Console.Title = $"Cluster Node ({((ExtendedActorSystem)system).Provider.DefaultAddress})";
+
+            var singletonManager = system.ActorOf(
+                props: ClusterSingletonManager.Props(
+                    singletonProps: Props.Create<MySingleton>(),
+                    terminationMessage: PoisonPill.Instance,
+                    //settings: ClusterSingletonManagerSettings.Create(system)),
+                    settings: new ClusterSingletonManagerSettings(
+                        "my-singleton",
+                        "singletoncontainer",
+                        TimeSpan.FromSeconds(10),
+                        TimeSpan.FromSeconds(3))),
+                name: "singleton");
+
+
+            var singletonProxy = system.ActorOf(ClusterSingletonProxy.Props(
+                singletonManagerPath: "/user/singleton",
+                //settings: ClusterSingletonProxySettings.Create(system)),
+                settings: new ClusterSingletonProxySettings("my-singleton", "singletoncontainer", TimeSpan.FromSeconds(3), 8192)),
+                name: "my-singleton-proxy");
+
+            system.Scheduler.ScheduleTellRepeatedly(
+                TimeSpan.Zero, 
+                TimeSpan.FromSeconds(4), 
+                singletonProxy, 
+                $"Message from process {Process.GetCurrentProcess().Id}", 
+                Nobody.Instance);
+
+            system.WhenTerminated.Wait();
+        }
+    }
+}
